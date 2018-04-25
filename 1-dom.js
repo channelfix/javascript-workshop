4043c102ca3b0707b6f248756e40c943ba9f163c
Arnelle Balane <arnellebalane@gmail.com>
2018-04-25 08:50:50 +0800

    initial commit

---
+++ 1-dom.js
@@ -0,0 +1,9 @@
+/**
+ *  1. Selecting elements
+ *  2. Navigating the DOM tree
+ *  3. Manipulating elements
+ *  4. Inserting new elements
+ *  5. Removing elements
+ *  6. Listening to events
+ *  7. Interacting with elements
+ */
---
+++ 2-ajax.js
@@ -0,0 +1,9 @@
+/**
+ *  1. XMLHttpRequest
+ *  2. Sending a GET request
+ *  3. Listening to state changes
+ *  4. Handling AJAX response
+ *  5. Aborting an AJAX request
+ *  6. Including cookies in requests
+ *  7. Sending a POST request
+ */
